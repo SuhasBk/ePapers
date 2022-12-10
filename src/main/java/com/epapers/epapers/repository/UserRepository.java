301e45e8a28cdd80490c476c7a9293dfc79405db
Suhas Bk <kowligi1998@gmail.com>
2022-12-10 21:34:19 +0530

    upgrade version

--- src/main/java/com/epapers/epapers/repository/UserRepository.java
+++ src/main/java/com/epapers/epapers/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package com.epapers.epapers.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import com.epapers.epapers.model.EpapersUser;
 
-@Repository
 public interface UserRepository extends MongoRepository<EpapersUser, String> {}
