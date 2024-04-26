76007cbcc7c3b5ff3deb8afbccdfbd4a8faf89b9
Suhas Bk <kowligi1998@gmail.com>
2024-04-25 22:13:14 -0500

    keep alive

--- src/main/java/com/epapers/epapers/schedulers/AppScheduler.java
+++ src/main/java/com/epapers/epapers/schedulers/AppScheduler.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.epapers.epapers.config.AppConfig;
+import com.epapers.epapers.service.EmailService;
 import com.epapers.epapers.telegram.EpapersBot;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -14,13 +19,16 @@ public class AppScheduler {
     @Autowired
     EpapersBot telegramBot;
 
-    // @Autowired
-    // EmailService emailService;
+    @Autowired
+    EmailService emailService;
+
+    @Autowired
+    HttpClient httpClient;
 
     // @Autowired
     // WebClient webClient;
 
-    // private static final String SERVER_URL = AppConfig.HOSTNAME;
+    private static final String SERVER_URL = AppConfig.HOSTNAME;
     // private static final String PORTFOLIO_URL = AppConfig.PORTFOLIO_URL;    // fly.io
     // private static final String CHATSTOMP_URL = AppConfig.CHATSTOMP_URL;    // render.com
 
@@ -38,38 +46,15 @@ public class AppScheduler {
     //         .block();
     // }
 
-    // @Scheduled(fixedDelay = 5, initialDelay = 5, timeUnit = TimeUnit.MINUTES)
-    // public void keepAlive() {
-    //     webClient
-    //         .get()
-    //         .uri(SERVER_URL)
-    //         .retrieve()
-    //         .toBodilessEntity()
-    //         .doOnSuccess(resp -> {
-    //             log.info("🎵 stayin' alive! 🎵");
-    //         })
-    //         .doOnError(err -> {
-    //             log.error("SOS! I AM DYING! SAVE ME!!!");
-    //             emailService.mailSOS();
-    //         })
-    //         .block();
+    @Scheduled(fixedDelay = 5, initialDelay = 5, timeUnit = TimeUnit.MINUTES)
+    public void keepAlive() throws Exception {
+        httpClient.send(HttpRequest.newBuilder().uri(new URI(SERVER_URL)).GET().build(), null);
+    }
 
-    //     webClient
-    //         .get()
-    //         .uri(CHATSTOMP_URL)
-    //         .retrieve()
-    //         .toBodilessEntity()
-    //         .doOnError(err -> {
-    //             log.error("SHOULD REVIVE CHATSTOMP!");
-    //             emailService.mailSOS();
-    //         })
-    //         .block();
-    // }
-
-     @Scheduled(fixedDelay = 10, initialDelay = 10, timeUnit = TimeUnit.MINUTES)
-     public void collectGarbage() {
-         System.gc();
-     }
+    @Scheduled(fixedDelay = 10, initialDelay = 10, timeUnit = TimeUnit.MINUTES)
+    public void collectGarbage() {
+        System.gc();
+    }
 
     // @Scheduled(cron = "0 0 0 * * ?", zone = "Asia/Kolkata")
     // public void refreshDB() {
